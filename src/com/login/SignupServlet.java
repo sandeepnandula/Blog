466c39375413bbd4aff3cf22f02fc0eb15638e5f
Gauri7 <gauri0794@gmail.com>
2016-11-11 19:10:22 +0530

    Unique key with valdiation

--- src/com/login/SignupServlet.java
+++ src/com/login/SignupServlet.java
@@ -5,11 +5,34 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.*;
 
+import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @SuppressWarnings("serial")
 public class SignupServlet extends HttpServlet {
+	
+	public static boolean checkUser(Key name){
+		boolean status = false;
+		DatastoreService ds=DatastoreServiceFactory.getDatastoreService();
+		
+		
+		try {
+			Entity emp = ds.get(name);
+			status = true;
+			//System.out.print(employee.toString());
+		} catch (EntityNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			status = false;
+			//System.out.println("Not found");
+		}
+		
+		return status;
+	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		com.google.appengine.api.datastore.DatastoreService ds=DatastoreServiceFactory.getDatastoreService();
@@ -19,7 +42,11 @@ public class SignupServlet extends HttpServlet {
 		String userName=req.getParameter("username");
 		String password=req.getParameter("password");
 		if(emailId!=null&&userName!=null&&password!=null){
+			
 			Entity user=new Entity("UserDetails");
+			Key key=KeyFactory.createKey("UserDetails", userName);
+			System.out.println(checkUser(key));
+			if(checkUser(key) == false){
 			user.setProperty("emailId", emailId);
 			user.setProperty("userName", userName);
 			user.setProperty("password", password);
@@ -27,9 +54,14 @@ public class SignupServlet extends HttpServlet {
 			out.print("<p>Your signup was successfull</p>");
 			out.print("<a href=\"ProfileServlet\"> Profile</a>");
 			out.print("<a href=\"LogoutServlet\"> Logout</a>");
+			}
 			
+			else{
+				out.print("<p>Please Enter Valid Details</p>");
+				
 			
 			}
+		}
 		else{
 			out.print("<p>Please Enter Valid Details</p>");
 		}
